d86526897592072350bda6a68d26ab5183ea9cf1
Vance Nelson <109315238+vncnlsn@users.noreply.github.com>
2024-05-30 13:33:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
         folder.addEventListener("click", function() {
             const notes = this.nextElementSibling;
             notes.classList.toggle("active");
-            const arrow = this.nextElementSibling.nextElementSibling;
+            const arrow = this.querySelector(".arrow");
             arrow.textContent = arrow.textContent === ">" ? "▼" : ">";
         });
     });
